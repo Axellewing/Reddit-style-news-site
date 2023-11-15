8c0b99a1b818e60846227b72499380b8fcedbd69
Axellewing <127880600+Axellewing@users.noreply.github.com>
2023-11-15 22:15:28 +0100

    Create basic index.html for my project

--- prod/views.py
+++ prod/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "index.html")
