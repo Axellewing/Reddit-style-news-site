6f061028fb597c86b97e2248b165aae8e9d64775
Axellewing <127880600+Axellewing@users.noreply.github.com>
2023-11-16 12:21:08 +0100

    Add profile model and configure media settings

--- tellit/urls.py
+++ tellit/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("prod/", include("prod.urls")),
+    path("", include("prod.urls")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
